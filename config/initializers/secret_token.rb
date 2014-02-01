a4e0564fb2a058207783bf37b107099382cb063a
Алексей Франк <alexeyfrank@MacBook-Pro-Aleksej-2.local>
2014-02-01 13:29:44 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Copad::Application.config.secret_key_base = 'b3d758b2f5716f07ea90f1e95836aadd9d132162db7da857baf88973c2eb2f79d75ea548b165f906b285fd0e7bbeb948247849353d07456b5e0db3b48d4f1f78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Copad::Application.config.session_store :cookie_store, key: '_copad_session'
