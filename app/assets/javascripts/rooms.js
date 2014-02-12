d6eff6f1335d254f72e296ae44eb7285de95993c
plugin73 <laplugin73@gmail.com>
2014-02-12 20:06:33 +0100

    attach ace

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -2,4 +2,8 @@ $(function (){
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/twilight");
   editor.getSession().setMode("ace/mode/javascript");
+
+  sharejs.open('hello', 'text', function(error, doc) {
+    doc.attach_ace(editor);
+  });
 });
